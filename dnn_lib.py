b83067c45175f3b437fb3f5394856f63b182a6a8
abdullayevzzz <abdullayev.1995@gmail.com>
2022-10-26 16:57:39 +0300

    working!

--- dnn_lib.py
+++ dnn_lib.py
@@ -1,76 +1,68 @@
 import numpy as np
 
 
-#TODO: normalize input data
 def data_normalize(raw_data):
-    return [] #TODO: implement
+    max_val = max([max(row) for row in raw_data])
+    return raw_data/max_val
 
 def sigmoid(Z):
     return 1 / (1 + np.exp(-Z))
 
-#TODO: implement relu function
 def relu(Z):
-    return []#TODO:implement
+    return np.maximum(0, Z)
 
-#TODO: complete implementation of function for single layer forward propagation
 #it should compute linear propagation based on input values and parameters
 #based on activation parameter should choose the proper non-linear activation function and apply calculations
 #return: activation A and linear Z matrix
 def single_layer_forward_propagation(A_prev, W_curr, b_curr, activation="relu"):
-    # TODO: calculation of linear propagation
-
+    Z = W_curr.dot(A_prev) + b_curr
     # selection of activation function
-    if activation is "relu":
+    if activation == "relu":
         activation_func = relu
-    elif activation is "sigmoid":
+    elif activation == "sigmoid":
         activation_func = sigmoid
     else:
         raise Exception('Non-supported activation function')
-    #TODO: calculate non-linear activation and return proper values
+    A = activation_func(Z)
     # return of calculated activation A and the intermediate Z matrix
+    return A, Z
 
-#TODO: implement full forward propagation for all layers return last lyaer A3 value and
-# all intermidiate matrixes A1, Z1... Z3 as memory list
+
 def full_forward_propagation(X, params_values):
-    #TODO: single_layer_forward_propagation with proper parameters 3 times for 3 layers
-    #TODO: create memory list with all intermidiate values A1, Z1... Z3
-    return []#TODO: return A3 and memory list
+    A1, Z1 = single_layer_forward_propagation(X, params_values['W1'], params_values['b1'], activation="relu")
+    A2, Z2 = single_layer_forward_propagation(A1, params_values['W2'], params_values['b2'], activation="relu")
+    A3, Z3 = single_layer_forward_propagation(A2, params_values['W3'], params_values['b3'], activation="sigmoid")
+    return A1, A2, A3, Z1, Z2, Z3
 
 def get_cost_value(Y_hat, Y):
     # number of examples
-    m = Y_hat.shape[1]
+    m = Y_hat.shape[0]
     # calculation of the cost according to the formula
     cost = -1 / m * (np.dot(Y, np.log(Y_hat).T) + np.dot(1 - Y, np.log(1 - Y_hat).T))
     return np.squeeze(cost)
+
 
 def sigmoid_backward(dA, Z):
     sig = sigmoid(Z)
     return dA * sig * (1 - sig)
 
-#TODO:implement backward relu function return dZ
 def relu_backward(dA, Z):
-    return [] #TODO:implement
+    return dA * (Z >= 0)  # elementwise multiply with boolean value
 
-#TODO: complete implementation of function for single layer backward propagation
 def single_layer_backward_propagation(dA_curr, W_curr, b_curr, Z_curr, A_prev, activation="relu"):
     # number of examples
     m = A_prev.shape[1]
-
     # selection of activation function
-    if activation is "relu":
+    if activation == "relu":
         backward_activation_func = relu_backward
-    elif activation is "sigmoid":
+    elif activation == "sigmoid":
         backward_activation_func = sigmoid_backward
     else:
         raise Exception('Non-supported activation function')
 
-    # TODO: calculation of the activation function derivative
-    dZ_curr=[] #TODO:complete, make calculations instead of empty declaration
-    # TODO: calculate dW_curr derivative of the matrix W as (dZ*A_prev.T)/m
-
-    #derivative of the vector b
+    dZ_curr = backward_activation_func(dA_curr, Z_curr)
+    dW_curr = dZ_curr.dot(A_prev.T)/m
     db_curr = np.sum(dZ_curr, axis=1, keepdims=True) / m
-    #TODO: calculate a dA_prev derivative of the matrix A_prev as W_curr.T*dZ_curr
-
-    #TODO: return dA_prev, dW_curr, db_curr
+    dA_prev = W_curr.T.dot(dZ_curr)
+    return dA_prev, dW_curr, db_curr
 
--- envSutisfaction.py
+++ envSutisfaction.py
@@ -2,10 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from dnn_lib import *
 
-#test22
+# test22
 learning_rate = 0.075
 num_iterations = 10
-#TODO: generate more data
+# TODO: generate more data
 raw_x = np.array([[20, 40, 30],
                   [45, 35, 25]])
 raw_y = np.array([[1],
@@ -17,10 +17,12 @@ HID_LAYER2 = 4
 OUTPUT_SIZE = 1
 np.random.seed(10)
 W1 = np.random.randn(HID_LAYER1, INPUT_SIZE) * 0.1
-W3=[]#TODO: generate random data for W2 and W3 weights
+W2 = np.random.randn(HID_LAYER2, HID_LAYER1) * 0.1
+W3 = np.random.randn(OUTPUT_SIZE, HID_LAYER2) * 0.1
 
 b1 = np.zeros((HID_LAYER1, 1))
-#TODO: generate b2 and b3 with zeros
+b2 = np.zeros((HID_LAYER2, 1))
+b3 = np.zeros((OUTPUT_SIZE, 1))
 
 print(raw_x)
 print(raw_y)
@@ -28,20 +30,22 @@ print(raw_x.shape)
 train_x = data_normalize(raw_x)
 print(train_x)
 
-#appends every iteration cost value, will use for making a plot
+# appends every iteration cost value, will use for making a plot
 cost_history = []
-#collection of all parameters
+# collection of all parameters
 param_values = {}
 param_values["W1"] = W1
 param_values["b1"] = b1
-#TODO: add also W2, b2, W3, b3
+param_values["W2"] = W2
+param_values["b2"] = b2
+param_values["W3"] = W3
+param_values["b3"] = b3
 
 # train
 for i in range(num_iterations):
-    #TODO: call full_forward_propagation with proper input values and param_values
-    A1=[], A2=[], A3=[], Z1=[], Z2=[] #TODO: get A3, A2, A1, Z2, Z1 from memory returned by full_forward_propagation
+    A1, A2, A3, Z1, Z2, Z3 = full_forward_propagation(train_x.T, param_values)
 
-    cost = 0 #TODO: call get_cost_value function for calculating the cost
+    cost = get_cost_value(A3.squeeze(), raw_y.squeeze())
     cost_history.append(cost)
 
     m = m = A2.shape[1]
@@ -49,9 +53,16 @@ for i in range(num_iterations):
     dW3 = np.dot(dZ3, A2.T) / m
     db3 = np.sum(dZ3, axis=1, keepdims=True) / m
     dA2 = np.dot(param_values["W3"].T, dZ3)
-    #TODO: call single_layer_backward_propagation 2 times for remaining 2 layers and get dA1, dW2, db2, dA0, dW1, db1
 
-    #TODO: update parameters W1, W2, W3, b1, b2, b3
+    dA1, dW2, db2 = single_layer_backward_propagation(dA2, W2, b2, Z2, A1, activation="relu")
+    dA0, dW1, db1 = single_layer_backward_propagation(dA1, W1, b1, Z1, train_x.T, activation="relu")
+    # TODO: update parameters W1, W2, W3, b1, b2, b3
+    W3 -= learning_rate * dW3
+    b3 -= learning_rate * db3
+    W2 -= learning_rate * dW2
+    b2 -= learning_rate * db2
+    W1 -= learning_rate * dW1
+    b1 -= learning_rate * db1
 
 
 print("Z3:", Z3)
@@ -65,9 +76,7 @@ print("W1", W1)
 print("b1", b1)
 print(cost)
 
-#TODO: create an input data for prediction
-#TODO: make prediction
-A_prediction=[]
+A_prediction = []
 print("A prediction", A_prediction)
 
 plt.plot(cost_history)
